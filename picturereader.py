06ca55923d126e5545b61324f96443461f0c958f
margooka <mkoka97@gmail.com>
2021-08-05 01:56:28 -0700

    initial contours found

--- picturereader.py
+++ picturereader.py
@@ -1,7 +1,43 @@
+import cv2 as cv
+
 # https://github.com/aleker/PyHanafudaClassifier
 # ^^^ check out this github
+# helpful video: https://www.youtube.com/watch?v=m-QPjO-2IkA
+# https://towardsdatascience.com/computer-vision-for-beginners-part-2-29b3f9151874
 
-def picreader(image):
 
-    lst = []  # list of cards
-    return lst
+def detect(image):
+    # grayscale
+    # blurred
+    # thresholded
+    # contour detection
+    # find number of cards
+
+    image = cv.imread(image)
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    blurred = cv.blur(gray, (10, 10))
+    #blurred = cv.medianBlur(gray, 5)
+    _, threshold = cv.threshold(blurred, 127, 225, cv.THRESH_BINARY)
+    #threshold2 = cv.adaptiveThreshold(blurred, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+    contours, hierarchy = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv.drawContours(image, contours, -1, (0, 255, 0), 3)
+    cv.imshow('Original image', image)
+    cv.imshow('Gray image', gray)
+    cv.imshow('Blurred image', blurred)
+    cv.imshow("Thresholded Image", threshold)
+    #cv.imshow("Adaptive Thresholded Image", threshold2)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+    return 1
+
+
+def identify(image):
+    # approximate corner point of each card
+    # transform card into flattened perspective image
+    # ? create color masks of each card (query image)
+    # ? compare mask to train images
+    return 1
+
+
+detect("images/IMG_8280.jpg")
--- scoring.py
+++ scoring.py
@@ -1,3 +1,5 @@
 # https://www.gamedesign.jp/flash/hanafuda/rule_e.html
 
+def calculate(lst):
+    return 1
 
